132e50c1f9959fb63f9d8473bffd8c506e0ea0e3
Lifo Fernandez <lifofernandez@gmail.com>
2016-09-06 22:50:26 -0300

    consigo login form

--- lib/Reservas/Manager.pm
+++ lib/Reservas/Manager.pm
@@ -12,6 +12,9 @@ set 'warnings'     => 1;
 
 set 'username' => 'admin';
 set 'password' => 'password';
+set 'layout'       => 'main';
+
+my $flash;
 
 use Reservas::Gestor;
 
@@ -35,26 +38,40 @@ get '/' => sub {
 };
 
 any ['get', 'post'] => '/login' => sub {
-   my $err;
- 
-   if ( request->method() eq "POST" ) {
-     # process form input
-     if ( params->{'username'} ne setting('username') ) {
-       $err = "Invalid username";
-     }
-     elsif ( params->{'password'} ne setting('password') ) {
-       $err = "Invalid password";
-     }
-     else {
-       session 'logged_in' => true;
-       set_flash('You are logged in.');
-       return redirect '/';
-     }
-  }
- 
-  # display login form
-  template 'login.tt', {
-    'err' => $err,
-  };
+	my $err;
+
+	if ( request->method() eq "POST" ) {
+	# process form input
+		if ( params->{'username'} ne setting('username') ) {
+			$err = "Invalid username";
+		} elsif ( params->{'password'} ne setting('password') ) {
+			$err = "Invalid password";
+	}else{
+			session 'logged_in' => true;
+			set_flash('You are logged in.');
+			return redirect '/';
+		}
+	}
+	# display login form
+	template 'login.tt', {
+		'err' => $err,
+	};
 };
+
+
+
+
+# Subfunciones
+sub set_flash {
+	my $message = shift;
+
+	$flash = $message;
+}
+
+sub get_flash {
+	my $msg = $flash;
+	$flash = "";
+	return $msg;
+}
+
 true;
