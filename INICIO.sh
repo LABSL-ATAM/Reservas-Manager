7837d9ddb63f719117544b99b5a3049191b58e19
MarxBro <allthemarxbrothers@gmail.com>
2016-09-26 19:21:30 -0300

    inicio script

---
+++ INICIO.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+######################################################################
+# Usar para iniciar el manager
+######################################################################
+
+[ $1 ] && OPCION="$1"
+[ ! $1 ] && OPCION="default"
+
+PUERTO=5000
+
+case "$OPCION" in 
+
+    start)
+        nohup plackup -E production -p $PUERTO bin/app.psgi &> /dev/null &
+    ;;
+
+    stop)
+        pkill --full plackup
+    ;;
+
+    dev)
+        nohup plackup -p $PUERTO bin/app.psgi &> /dev/null &
+    ;;
+
+    default)
+        #echo "Por defecto se cierran todas las instancias de plackup y se ejecuta el entorno de desarrollo."
+        pkill --full plackup
+        nohup plackup -p $PUERTO bin/app.psgi &> /dev/null &
+    ;;
+    *)
+        echo "Uso: [start|stop|dev]. Sin opciones ejecuta el entorno de desarrollo."
+    ;;
+esac
+
+
+
+echo "Listo" &&
+exit 0
